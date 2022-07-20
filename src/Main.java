37785c75bf7e74ea5b982cd61185c966b885c1a4
Debora <deboraalbuquerque.mosy@gmail.com>
2022-07-20 17:48:11 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -18,8 +20,42 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devDebora = new Dev();
+        devDebora.setNome("Debora");
+        devDebora.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Debora" + devDebora.getConteudosInscritos());
+        devDebora.progredir();
+        devDebora.progredir();
+        System.out.println("-")
+        System.out.println("Conteudos Inscritos Debora" + devDebora.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Debora" + devDebora.getConteudosConcluidos());
+        System.out.println("XP:" + devDebora.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devCristina = new Dev();
+        devCristina.setNome("Cristina");
+        devCristina.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devCristina.getConteudosInscritos());
+        devCristina.progredir();
+        devCristina.progredir();
+        devCristina.progredir();
+        System.out.println("-")
+        System.out.println("Conteudos Inscritos" + devCristina.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos" + devCristina.getConteudosConcluidos());
+        System.out.println("XP:" + devCristina.calcularTotalXp());
+
     }
 }
